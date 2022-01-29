6d1d4032691e6c7477dae95e3824137ad1f19c03
Mohit R <mohit.jan92@gmail.com>
2022-01-30 00:23:45 +0530

    Updates

---
+++ precache-manifest.47d9654df9b02aad4eb176ecb54df938.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd4dd883a2c76e30afe8dfa96496bff",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "3e8070adf74f2914cacb",
+    "url": "/myportfolio/static/css/2.0e0117fa.chunk.css"
+  },
+  {
+    "revision": "35c039a1d1a8a721a16d",
+    "url": "/myportfolio/static/css/main.90fae649.chunk.css"
+  },
+  {
+    "revision": "3e8070adf74f2914cacb",
+    "url": "/myportfolio/static/js/2.770233e2.chunk.js"
+  },
+  {
+    "revision": "44f515a80a48c76af8012084c85006fd",
+    "url": "/myportfolio/static/js/2.770233e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c039a1d1a8a721a16d",
+    "url": "/myportfolio/static/js/main.396052b3.chunk.js"
+  },
+  {
+    "revision": "331faa8402dd0f6ae6f6",
+    "url": "/myportfolio/static/js/runtime-main.96a916e9.js"
+  },
+  {
+    "revision": "0870bbed1f10e0862e6f9db255a04278",
+    "url": "/myportfolio/static/media/Dummy_resume.0870bbed.pdf"
+  },
+  {
+    "revision": "1fd22a50b4b82e5308b74569bf2ae6e6",
+    "url": "/myportfolio/static/media/JR_Img.1fd22a50.JPG"
+  },
+  {
+    "revision": "807a90c68fcdc142cfff49aad7cad10d",
+    "url": "/myportfolio/static/media/JR_app.807a90c6.JPG"
+  },
+  {
+    "revision": "b8ac108303c5dee9c302d9d7186fdd94",
+    "url": "/myportfolio/static/media/TC_3.b8ac1083.jpg"
+  },
+  {
+    "revision": "622cdfc918495fd59b007ad1df162b3c",
+    "url": "/myportfolio/static/media/TC_4.622cdfc9.jpg"
+  },
+  {
+    "revision": "4c9f0952fec07764b1b8237c4e3145ca",
+    "url": "/myportfolio/static/media/VAG.4c9f0952.JPG"
+  },
+  {
+    "revision": "3b85065c816be4bb07ebab64271f18a0",
+    "url": "/myportfolio/static/media/covid.3b85065c.jpg"
+  },
+  {
+    "revision": "fddfd2a16f0b53c1383025195f4ef7ea",
+    "url": "/myportfolio/static/media/cvdtracker.fddfd2a1.JPG"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/myportfolio/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/myportfolio/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/myportfolio/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/myportfolio/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/myportfolio/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/myportfolio/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/myportfolio/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/myportfolio/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/myportfolio/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/myportfolio/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/myportfolio/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/myportfolio/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/myportfolio/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/myportfolio/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/myportfolio/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "83c62003eb89ca50fe531b49f318004e",
+    "url": "/myportfolio/static/media/head-6.83c62003.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/myportfolio/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/myportfolio/static/media/map-image.f40ed5b1.png"
+  }
+]);
